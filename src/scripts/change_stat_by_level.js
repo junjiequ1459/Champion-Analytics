22740404ce75df434d79f8e5f49a7553f6d99d16
rexqu <junjiequ123@gmail.com>
2023-01-26 12:20:49 -0500

    removed console.log

--- src/scripts/change_stat_by_level.js
+++ src/scripts/change_stat_by_level.js
@@ -2,34 +2,78 @@ export function changeStatByLevel(data) {
   let select = document.querySelector("#number-select");
   select.addEventListener("change", function () {
     let selectedNumber = select.value;
-    console.log(selectedNumber)
+    // console.log(selectedNumber)
     if (selectedNumber === "1") {
-      document.querySelector("#c_health").innerText = `Health : ${JSON.stringify(data.hp)}`;
-      document.querySelector("#c_mana").innerText = `Mana : ${JSON.stringify(data.mp)}`;
-      document.querySelector("#c_health_regen").innerText = `HealthRegen : ${JSON.stringify(data.hpregen)}`;
-      document.querySelector("#c_mana_regen").innerText = `ManaRegen : ${JSON.stringify(data.mpregen)}`;
-      document.querySelector("#c_armor").innerText = `Armor : ${JSON.stringify(data.armor)}`;
-      document.querySelector("#c_attack_damage").innerText = `AttackDamage : ${JSON.stringify(data.attackdamage)}`;
-      document.querySelector("#c_magic_resist").innerText = `MagicResist : ${JSON.stringify(data.spellblock)}`;
-      document.querySelector("#c_crit").innerText = `Crit : ${JSON.stringify(data.crit)}`;
-      document.querySelector("#c_move_speed").innerText = `MoveSpeed : ${JSON.stringify(data.movespeed)}`;
-      document.querySelector("#c_attack_range").innerText = `AttackRange : ${JSON.stringify(data.attackrange)}`;
+      document.querySelector(
+        "#c_health"
+      ).innerText = `Health : ${JSON.stringify(data.hp)}`;
+      document.querySelector("#c_mana").innerText = `Mana : ${JSON.stringify(
+        data.mp
+      )}`;
+      document.querySelector(
+        "#c_health_regen"
+      ).innerText = `HealthRegen : ${JSON.stringify(data.hpregen)}`;
+      document.querySelector(
+        "#c_mana_regen"
+      ).innerText = `ManaRegen : ${JSON.stringify(data.mpregen)}`;
+      document.querySelector("#c_armor").innerText = `Armor : ${JSON.stringify(
+        data.armor
+      )}`;
+      document.querySelector(
+        "#c_attack_damage"
+      ).innerText = `AttackDamage : ${JSON.stringify(data.attackdamage)}`;
+      document.querySelector(
+        "#c_magic_resist"
+      ).innerText = `MagicResist : ${JSON.stringify(data.spellblock)}`;
+      document.querySelector("#c_crit").innerText = `Crit : ${JSON.stringify(
+        data.crit
+      )}`;
+      document.querySelector(
+        "#c_move_speed"
+      ).innerText = `MoveSpeed : ${JSON.stringify(data.movespeed)}`;
+      document.querySelector(
+        "#c_attack_range"
+      ).innerText = `AttackRange : ${JSON.stringify(data.attackrange)}`;
       return data;
     } else {
       let dupObj = { ...data };
 
       dupObj = { ...addStats(dupObj, select.value) };
 
-      document.querySelector("#c_health").innerText = `Health : ${JSON.stringify(dupObj.hp)}`;
-      document.querySelector("#c_mana").innerText = `Mana : ${JSON.stringify(dupObj.mp)}`;
-      document.querySelector("#c_health_regen").innerText = `HealthRegen : ${JSON.stringify((dupObj.hpregen).toFixed(1))}`;
-      document.querySelector("#c_mana_regen").innerText = `ManaRegen : ${JSON.stringify((dupObj.mpregen).toFixed(1))}`;
-      document.querySelector("#c_armor").innerText = `Armor : ${JSON.stringify((dupObj.armor).toFixed(1))}`;
-      document.querySelector("#c_attack_damage").innerText = `AttackDamage : ${JSON.stringify(dupObj.attackdamage)}`;
-      document.querySelector("#c_magic_resist").innerText = `MagicResist : ${JSON.stringify((dupObj.spellblock).toFixed(1))}`;
-      document.querySelector("#c_crit").innerText = `Crit : ${JSON.stringify(dupObj.crit)}`;
-      document.querySelector("#c_move_speed").innerText = `MoveSpeed : ${JSON.stringify(dupObj.movespeed)}`;
-      document.querySelector("#c_attack_range").innerText = `AttackRange : ${JSON.stringify(dupObj.attackrange)}`;
+      document.querySelector(
+        "#c_health"
+      ).innerText = `Health : ${JSON.stringify(dupObj.hp)}`;
+      document.querySelector("#c_mana").innerText = `Mana : ${JSON.stringify(
+        dupObj.mp
+      )}`;
+      document.querySelector(
+        "#c_health_regen"
+      ).innerText = `HealthRegen : ${JSON.stringify(
+        dupObj.hpregen.toFixed(1)
+      )}`;
+      document.querySelector(
+        "#c_mana_regen"
+      ).innerText = `ManaRegen : ${JSON.stringify(dupObj.mpregen.toFixed(1))}`;
+      document.querySelector("#c_armor").innerText = `Armor : ${JSON.stringify(
+        dupObj.armor.toFixed(1)
+      )}`;
+      document.querySelector(
+        "#c_attack_damage"
+      ).innerText = `AttackDamage : ${JSON.stringify(dupObj.attackdamage)}`;
+      document.querySelector(
+        "#c_magic_resist"
+      ).innerText = `MagicResist : ${JSON.stringify(
+        dupObj.spellblock.toFixed(1)
+      )}`;
+      document.querySelector("#c_crit").innerText = `Crit : ${JSON.stringify(
+        dupObj.crit
+      )}`;
+      document.querySelector(
+        "#c_move_speed"
+      ).innerText = `MoveSpeed : ${JSON.stringify(dupObj.movespeed)}`;
+      document.querySelector(
+        "#c_attack_range"
+      ).innerText = `AttackRange : ${JSON.stringify(dupObj.attackrange)}`;
       return dupObj;
     }
   });
